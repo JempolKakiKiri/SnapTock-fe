ec5ae52f53a6e8d9db1fa81d388d17df2b01aa04
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-08-22 08:59:32 +0700

    fix: adjust layout and spacing in DashboardHeader component

--- src/features/dashboard/components/DashboardHeader.tsx
+++ src/features/dashboard/components/DashboardHeader.tsx
@@ -7,35 +7,29 @@ const DashboardHeader = () => {
         Overview / Today
       </span>
 
-      <div className="flex items-center gap-12">
-        <input
-          type="text"
-          placeholder="Search inventory..."
-          className="h-10 w-60 max-w-60 rounded-xl border border-gray-300 px-3 text-xs outline-none placeholder:text-gray-400 focus:border-green-500"
-        />
-        <div className="flex gap-4">
-          <button
-            type="button"
-            className="text-black transition-colors hover:text-gray-900 cursor-pointer"
-          >
-            <BellRing
-              width={32}
-              height={32}
-              className="hover:scale-105 transition-transform duration-200"
-            />
-          </button>
+      <div className="flex items-center gap-4">
 
-          <button
-            type="button"
-            className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-200 cursor-pointer"
-          >
-            <UserRound
-              width={32}
-              height={32}
-              className="hover:scale-105 transition-transform duration-200"
-            />
-          </button>
-        </div>
+        <button
+          type="button"
+          className="text-black transition-colors hover:text-gray-900 cursor-pointer"
+        >
+          <BellRing
+            width={32}
+            height={32}
+            className="hover:scale-105 transition-transform duration-200"
+          />
+        </button>
+
+        <button
+          type="button"
+          className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-200 cursor-pointer"
+        >
+          <UserRound
+            width={32}
+            height={32}
+            className="hover:scale-105 transition-transform duration-200"
+          />
+        </button>
       </div>
     </header>
   );
