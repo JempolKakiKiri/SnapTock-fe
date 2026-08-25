5ffab249fe5e75b8026f98e866c0de337ccf4488
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-08-25 20:43:41 +0700

    feat: simplify headers by removing unused icons and adjusting layout

--- src/features/dashboard/components/DashboardHeader.tsx
+++ src/features/dashboard/components/DashboardHeader.tsx
@@ -1,35 +1,9 @@
-import { BellRing, UserRound } from "lucide-react";
-
 const DashboardHeader = () => {
   return (
     <header className="flex h-18 items-center justify-between border-b-2 bg-white px-6">
-      <span className="text-base font-inter-500 text-black">
+      <p className="pl-12 md:pl-0 text-base font-inter-500 text-black">
         Overview / Today
-      </span>
-
-      <div className="flex items-center gap-4">
-        <button
-          type="button"
-          className="text-black transition-colors hover:text-gray-900 cursor-pointer"
-        >
-          <BellRing
-            width={32}
-            height={32}
-            className="hover:scale-105 transition-transform duration-200"
-          />
-        </button>
-
-        <button
-          type="button"
-          className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-200 cursor-pointer"
-        >
-          <UserRound
-            width={32}
-            height={32}
-            className="hover:scale-105 transition-transform duration-200"
-          />
-        </button>
-      </div>
+      </p>
     </header>
   );
 };
